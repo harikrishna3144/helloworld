414b2d2822c4e41ca59c77022176f112203c6459
harikrishna3144 <mopuriharikrishna3144@gmail.com>
2023-05-23 23:05:21 +0530

    helloworld

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import UserText from './components/UserText'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserText/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserText/index.js
@@ -0,0 +1,16 @@
+
+import {Component} from 'react'
+import './index.css'
+
+class UserText extends Component {
+
+    render(){
+        return(
+            <>
+            <h1 className='heading'>Hello World !!!</h1>
+            <p className='description'>I'm Hari Krishna </p>
+            </>
+        )
+    }
+}
+export default UserText
